df4825d92790ff40731dd00f1ee0fa966b882930
stinamarkan <stina895@hotmail.com>
2016-11-24 15:15:56 +0100

    Update Rectangle.java

--- geometri/Rectangle.java
+++ geometri/Rectangle.java
@@ -6,19 +6,45 @@ public class Rectangle extends Square {
 
     private int height;
 
+    /**
+     * 
+     * @param x Distance right from origo.
+     * @param y Distance down from origo.
+     * @param width Width of the rectangle.
+     * @param height Height of the rectangle.
+     * @param c Color of the rectangle.
+     * @throws <tt>IllegalPositionException</tt> if any coordinate is negative.
+     */
     public Rectangle(int x, int y, int width, int height, Color c) throws IllegalPositionException {}
+    
+    /**
+     * A constructor with four parameters.
+     * @param f The geometrical form for the rectangle.
+     * @param width The width of the rectangle.
+     * @param height The height of the rectangle.
+     * @param c Color of the rectangle.
+     */
     public Rectangle(GeometricalForm f, int width, int height, Color c) {}
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getArea() {
         return 0;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getPerimeter() {
         return 0;
     }
-
+    
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getHeight() {
         return 0;
